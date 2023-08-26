363b2ab7643d007c36d7f4c1c40d0f52285ff244
Jian <jian1.liu@wur.nl>
2023-08-26 10:19:30 +0200

    stopping trying snakemake maybe overkill

---
+++ scripts/fetch_soil.py
@@ -0,0 +1,3 @@
+from pcse.fileinput import CABOFileReader
+# load the soil information
+soild = CABOFileReader('../data/ec3 sandyloam.soil')
---
+++ scripts/fetch_weather.py
@@ -0,0 +1,4 @@
+from agera5tools.wdp import AgERA5WeatherDataProvider
+# Indian field experiment location coordinates 
+
+wdp = AgERA5WeatherDataProvider(longitude=73.13, latitude=23.84)
