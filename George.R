cbc5c62901876b7569725b2ce88a1eb9775642f9
Jian <jian1.liu@wur.nl>
2023-09-08 15:55:45 +0200

    copied George temp function

--- George.R
+++ George.R
@@ -32,12 +32,13 @@ dev.off()
 
 # Temperature model --------------
 
-T_L <- 18.85
-T_H <- 29.85
-T_AL <- 19726.85
-T_AH <- 59726.85
-x <- seq(2, 50, by = 1)
-Tt <- (1 + exp(T_AL/x - T_AL/T_L) + exp(T_AH/T_H - T_AH/x)) ^ -1
+T_L <- 292
+T_H <- 303
+T_AL <- 20000
+T_AH <- 60000
+x <- seq(1, 4000, by = 1)
+x.t <- x/100
+Tt <- (1 + exp(T_AL/(x.t+273) - T_AL/T_L) + exp(T_AH/T_H - T_AH/(x.t+273))) ^ -1
 plot(Tt)
 
 Tmin <- 2
@@ -46,9 +47,9 @@ Teff <- (2 * (x-Tmin)^a * (25- Tmin)^a - (x - Tmin) ^ (2*a))/((25-Tmin)^ (2*a))
 plot(Teff)
 
 
-Mod <- function(t, x, y, par) {
+Mod <- function(t, y, par) {
   with(as.list(c(par, y)), {
-    Teff <- (2 * (x-Tmin)^a * (25- Tmin)^a - (x - Tmin) ^ (2*a))/((25-Tmin)^ (2*a))
+    Teff <- (1 + exp(T_AL/(x.t+273) - T_AL/T_L) + exp(T_AH/T_H - T_AH/(x.t+273))) ^ -1
     
     dM <- r*M*(1 - M/Mx) * Teff
     
@@ -58,5 +59,32 @@ Mod <- function(t, x, y, par) {
     return(list(c(dM, dsr, dsMx, dsM0)))
   })
 }
+parms1 <- c(r = 0.25, Mx = 10, M = 0.1, sr = 0, sMx = 0, sM0 = 1)
+end.time <- 4000
+time.vec <- 0:(end.time)
 out.model <- ode(y = c(parms1["M"], parms1["sr"], parms1["sMx"], parms1["sM0"]),
                  times = time.vec, func = Mod, parms = c(parms1["r"], parms1["Mx"]) )
+Mod
+
+### Temperature response curve
+TL <- 292
+TH <- 303
+TAL <- 20000
+TAH <- 60000
+
+y.vec <- seq(from = 1, to = 4000, by = 1) ### Vector with temperature points
+r.adapt <- y.vec ### Same no. of elements
+for (y in y.vec) {
+  temp.t <- y/100
+  r.adapt[y] <- (1 + exp(TAL/(temp.t + 273) - TAL/TL) + exp(TAH/TH - TAH/(temp.t + 273) ))^-1
+}
+
+plot(r.adapt)
+### Plot
+filename.pic <- "Fig-Temp.png"
+header.pic <- "Temperature response curve"
+png(filename = filename.pic, width = 4, height = 4, units = 'in', res = 300)
+plot(y.vec/100, r.adapt, main = header.pic, xlab = "temperature (degrees Celsius)", 
+     ylab = "factor", xlim = c(0,40), type = "l", lwd = 2)
+dev.off()
+
