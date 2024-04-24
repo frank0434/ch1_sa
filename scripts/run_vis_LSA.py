6dc15f891190a6f1954a321b0407e9b22d18100d
Jian <jian1.liu@wur.nl>
2024-04-24 21:24:55 +0200

    put DVS 0 and 1 on the graph

--- scripts/run_vis_LSA.py
+++ scripts/run_vis_LSA.py
@@ -95,6 +95,10 @@ xlimt_upper = no_ofdays - 1
 pointsize = 1
 subplotlab_x = 0.05
 subplotlab_y = 0.85
+
+# DVS values 
+emergence_date = output_df[output_df['DVS'] == 0]['DVS'].drop_duplicates()
+tuberintiation_date = output_df[output_df['DVS'] == 1]['DVS'].drop_duplicates()
 import matplotlib.gridspec as gridspec
 
 fig = plt.figure(figsize=(8, 9))
@@ -111,10 +115,12 @@ sc = ax1.scatter(output_df.index, output_df[output_var], c=output_df['value'], s
 ax1.set_xlabel('')
 ax1.set_ylabel(output_var)
 ax1.vlines(xlimt_upper, 0, ylimt_upper, color='red', linestyle='--')
+ax1.axvline(emergence_date.index, color='green', linestyle='-')
+ax1.axvline(tuberintiation_date.index, color='green', linestyle='-')
 ax1.annotate('Illustration on the right', xy=(xlimt_upper, ylimt_upper),
               xytext=(xlimt_upper, ylimt_upper + 1), 
              arrowprops=dict(facecolor='black', shrink=0.0), ha = 'center')
-ax1.text(subplotlab_x, subplotlab_y, 'a)', transform=ax1.transAxes, size=20, weight='bold')
+ax1.text(subplotlab_x, subplotlab_y, 'a)', transform=ax1.transAxes, size=config.subplot_fs, weight='bold')
 # Second subplot
 ax2 = fig.add_subplot(gs[0, 1])
 # Get the final day of output_df
@@ -125,7 +131,7 @@ sc2 = ax2.scatter(final_day_df['value'], final_day_df[output_var], c=final_day_d
 fig.colorbar(sc2, ax=ax2)
 ax2.set_xlabel(param_name)
 ax2.set_ylabel(output_var)
-ax2.text(subplotlab_x, subplotlab_y, 'b)', transform=ax2.transAxes, size=20, weight='bold')
+ax2.text(subplotlab_x, subplotlab_y, 'b)', transform=ax2.transAxes, size=config.subplot_fs, weight='bold')
 # Third subplot
 ax3 = fig.add_subplot(gs[1, 0])
 cmap = plt.get_cmap('viridis')
@@ -139,7 +145,7 @@ ax3.vlines(xlimt_upper, 0, ylimt_upper, color='red', linestyle='--')
 ax3.annotate('Illustration on the right', xy=(xlimt_upper, ylimt_upper),
               xytext=(xlimt_upper, ylimt_upper + 1), 
              arrowprops=dict(facecolor='black', shrink=0.0), ha = 'center')
-ax3.text(subplotlab_x, subplotlab_y, 'c)', transform=ax3.transAxes, size=20, weight='bold')
+ax3.text(subplotlab_x, subplotlab_y, 'c)', transform=ax3.transAxes, size=config.subplot_fs, weight='bold')
 
 # Fourth subplot
 # from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -155,7 +161,7 @@ sc4 = ax4.scatter(final_day_df_wirdo['value'], final_day_df_wirdo[output_var], c
 fig.colorbar(sc4, ax=ax4)
 ax4.set_xlabel(param_name_wirdo)
 ax4.set_ylabel(output_var)
-ax4.text(subplotlab_x, subplotlab_y, 'd)', transform=ax4.transAxes, size=20, weight='bold')
+ax4.text(subplotlab_x, subplotlab_y, 'd)', transform=ax4.transAxes, size=config.subplot_fs, weight='bold')
 
 # Fifth subplot output_df_wirdo
 ax5 = fig.add_subplot(gs[2, 0])
@@ -165,7 +171,7 @@ sc5 = ax5.scatter(output_df_no_effect.index, output_df_no_effect[output_var], c=
 # fig.colorbar(sc, ax=ax1)
 ax5.set_xlabel('DAP')
 ax5.set_ylabel(output_var)
-ax5.text(subplotlab_x, subplotlab_y, 'e)', transform=ax5.transAxes, size=20, weight='bold')
+ax5.text(subplotlab_x, subplotlab_y, 'e)', transform=ax5.transAxes, size=config.subplot_fs, weight='bold')
 xlimt_upper = (no_ofdays - 1)/2 if config.run_NL_conditions else no_ofdays - 1
 ax5.vlines(xlimt_upper, 0, ylimt_upper, color='red', linestyle='--')
 ax5.annotate('Illustration on the right', xy=(xlimt_upper, ylimt_upper ),
@@ -181,7 +187,7 @@ sc6 = ax6.scatter(final_day_df_no_effect['value'], final_day_df_no_effect[output
 fig.colorbar(sc6, ax=ax6)
 ax6.set_xlabel(param_name_no_effect)
 ax6.set_ylabel(output_var)
-ax6.text(subplotlab_x, subplotlab_y, 'f)', transform=ax6.transAxes, size=20, weight='bold')
+ax6.text(subplotlab_x, subplotlab_y, 'f)', transform=ax6.transAxes, size=config.subplot_fs, weight='bold')
 
 
 plt.savefig(f'{config.p_out_LSA}/{output_var}_mainText_{config.LSA_sample_size}.png', dpi = 300, bbox_inches='tight', pad_inches=0.1)
