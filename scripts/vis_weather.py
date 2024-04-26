a8c0e87af8509e15ba66fb6693b2e974594772b4
Jian <jian1.liu@wur.nl>
2024-04-26 10:29:00 +0200

    hardcode the date filter

--- scripts/vis_weather.py
+++ scripts/vis_weather.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import config
 from pcse.fileinput import ExcelWeatherDataProvider
 import Todo_before_writing as daylength_data
+from datetime import datetime
 # Load the data
 wdp_NL = ExcelWeatherDataProvider('../data_raw\\350_weatherfile_2021.xlsx')
 wdp_IND = ExcelWeatherDataProvider('../data_raw\\India2022_23.xlsx')
@@ -16,14 +17,10 @@ df_NL.set_index('DAY', inplace=True)
 df_IND.set_index('DAY', inplace=True)
 # %%
 # Filter the DataFrame
-from datetime import datetime
-
-# Convert the strings to datetime.date objects
-SIMULATION_START_DATE = datetime.strptime(config.SIMULATION_START_DATE, '%Y-%m-%d').date()
-SIMULATION_END_DATE_real = datetime.strptime(config.SIMULATION_END_DATE_real, '%Y-%m-%d').date()
 
 # Now you can filter the DataFrame
-df_IND = df_IND[(df_IND.index >= SIMULATION_START_DATE) & (df_IND.index <= SIMULATION_END_DATE_real)]
+df_IND = df_IND[(df_IND.index >= datetime.strptime("2022-11-10", '%Y-%m-%d').date()) & (df_IND.index <= datetime.strptime("2023-02-28",'%Y-%m-%d').date())]
+
 df_NL = df_NL[(df_NL.index >= datetime.strptime("2021-04-20", '%Y-%m-%d').date()) & (df_NL.index <= datetime.strptime("2021-09-30",'%Y-%m-%d').date())]
 # %%
 
@@ -53,7 +50,7 @@ ax1.set_ylabel('Temperature (°C)')
 ax1.set_xticklabels('')
 
 ax1.set_title('Indian Weather Data')
-ax1.text(indicatation_text_x, indicatation_text_y, 'b)', transform=ax1.transAxes, size=16, weight='bold')
+ax1.text(indicatation_text_x, indicatation_text_y, 'b)', transform=ax1.transAxes, size=config.subplot_fs, weight='bold')
 
 ax2 = fig.add_subplot(gs[0, 0], sharey=ax1)
 ax2.plot(df_NL.index, df_NL['TMAX'], label='Maximum Temperature', color='r')
@@ -64,23 +61,23 @@ ax2.set_xticklabels('')
 ax2.set_ylabel('Temperature (°C)')
 ax2.legend()
 ax2.set_title('Dutch Weather Data')
-ax2.text(indicatation_text_x, indicatation_text_y, 'a)', transform=ax2.transAxes, size=16, weight='bold')
+ax2.text(indicatation_text_x, indicatation_text_y, 'a)', transform=ax2.transAxes, size=config.subplot_fs, weight='bold')
 
 
 # Second subplot
 # Third subplot
 ax3 = fig.add_subplot(gs[1, 1])
-ax3.plot(df_IND.index, df_IND['IRRAD']/j_to_mj, label='IRRAD', color='b')
+ax3.plot(df_IND.index, df_IND['IRRAD']/j_to_mj, label='Irraditation', color='b')
 ax3.xaxis.set_major_locator(mdates.MonthLocator())
 ax3.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
 # add day length 
 daylength_data.df_ind['daylength'].plot(label='Daylength', color='black', ax=ax3)
 # Create a second y-axis for the third subplot
 ax3b = ax3.twinx()
-ax3b.bar(df_IND.index, df_IND['RAIN'] * 10, label='RAIN', color='r')
+ax3b.bar(df_IND.index, df_IND['RAIN'] * 10, label='Rain', color='r')
 ax3b.set_ylabel('Rainfall (mm)', color='r')
 
-ax3.text(indicatation_text_x, indicatation_text_y, 'd)', transform=ax3.transAxes, size=16, weight='bold')
+ax3.text(indicatation_text_x, indicatation_text_y, 'd)', transform=ax3.transAxes, size=config.subplot_fs, weight='bold')
 ax3.legend()
 
 
@@ -91,13 +88,14 @@ ax4.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
 # ax4.set_ylabel('Irradiation', color='b')
 ax4b = ax4.twinx()
 ax4b.get_shared_y_axes().join(ax4b, ax3b)
-ax4b.bar(df_NL.index, df_NL['RAIN'] * 10, label='RAIN', color='r')
+ax4b.bar(df_NL.index, df_NL['RAIN'] * 10, label='Rain', color='r')
 # Add letter indication
-ax4.text(indicatation_text_x, indicatation_text_y, 'c)', transform=ax4.transAxes, size=16, weight='bold')
+ax4.text(indicatation_text_x, indicatation_text_y, 'c)', transform=ax4.transAxes, size=config.subplot_fs, weight='bold')
 ax4.set_ylabel('Irradiation (MJ/m²/day)', color='b')
 ax4b.legend()
 ax4b.set_yticklabels('')
-plt.savefig(f'../output/weather_data.png', dpi = 300)
-plt.savefig(f'../output/weather_data.svg', dpi = 600)
+
+plt.savefig(f'../output/weather_data.png', dpi = 300, bbox_inches='tight')
+plt.savefig(f'../output/weather_data.svg', dpi = 600, bbox_inches='tight')
 plt.show()
 # %%
