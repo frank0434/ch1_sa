82438b4fc11c0be1703be086eca12c0424126849
Jian <jian1.liu@wur.nl>
2024-06-21 09:15:13 +0200

    made a comparison between two conditions for TWSO AT FINAL DAY ONLY

--- scripts/run_vis_GSA_barh.py
+++ scripts/run_vis_GSA_barh.py
@@ -44,6 +44,52 @@ def to_df(self):
         {k: v for k, v in self.items() if k in keys_to_include}, index = config.params_of_interests
     )
 
+
+# %% Produce a fig that shows the last day of TWSO for both conditions with bar chart 
+# load NLD manually
+with open(f'C:/Users/liu283/GitRepos/ch1_SA/output_NL/daySi_32768/Saltelli_160_TWSO.pkl', 'rb') as f:
+    Si_NLD = pickle.load(f)
+Si_df_NLD = to_df(Si_NLD['si_day_160_TWSO'])
+df_sorted_NLD = Si_df_NLD.sort_values(by='ST', ascending=True)
+order = df_sorted_NLD.index
+conf_cols = df_sorted_NLD.columns.str.contains('_conf')
+confs = df_sorted_NLD.loc[:, conf_cols]
+confs.columns = [c.replace('_conf', "") for c in confs.columns]
+Sis_NLD = df_sorted_NLD.loc[:, ~conf_cols]
+
+# load indian 
+Si_IND = load_data(105, 'TWSO', 'Saltelli')
+Si_df_IND = to_df(Si_IND['si_day_105_TWSO'])
+
+df_sorted_new = Si_df_IND.reindex(order)
+conf_cols = df_sorted_new.columns.str.contains('_conf')
+confs = df_sorted_new.loc[:, conf_cols]
+confs.columns = [c.replace('_conf', "") for c in confs.columns]
+Sis_IND = df_sorted_new.loc[:, ~conf_cols]
+# Plot sorted DataFrame
+# %%
+# Save the original default font size
+original_font_size = plt.rcParams['font.size']
+# Set the new default font size
+plt.rcParams['font.size'] = 18
+color = ['red', 'blue']
+fig, axs = plt.subplots(1, 2, figsize=(7, 5), sharex=True)
+plt.subplots_adjust(wspace=0.15, hspace=0.05)
+# NLD
+barplot = Sis_NLD.plot(kind='barh', yerr=confs , width = 0.9, ax=axs[0],
+                   legend=False, color = color)
+# indian
+barplot = Sis_IND.plot(kind='barh', yerr=confs , width = 0.9, ax=axs[1],
+                   legend=True, color = color)
+                   
+yticklabels =  [item.get_text() for item in axs[0].get_yticklabels()]
+new_yticklabels = [config.label_map.get(label, label) for label in yticklabels]
+axs[0].set_yticklabels(new_yticklabels)
+axs[1].set_yticklabels([])
+# barplot
+plt.xlim(0, 1)
+plt.show()
+plt.rcParams['font.size'] = original_font_size
 # %%
 differences = config.days_s2
 samplesize = 32768
@@ -200,7 +246,6 @@ plt.show()
 # %% make graph for slides
 # Save the original default font size
 original_font_size = plt.rcParams['font.size']
-
 # Set the new default font size
 plt.rcParams['font.size'] = 22
 var = 'LAI'
