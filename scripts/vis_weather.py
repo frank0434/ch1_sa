ab59efa9c9080832ef2f135b038b5c172477035a
Jian <jian1.liu@wur.nl>
2024-07-18 16:02:23 +0200

    remove the rain and radiation graphs

--- scripts/vis_weather.py
+++ scripts/vis_weather.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import config
 from pcse.fileinput import ExcelWeatherDataProvider
-import Todo_before_writing as daylength_data
+# import Todo_before_writing as daylength_data
 from datetime import datetime
 # Load the data
 wdp_NL = ExcelWeatherDataProvider('../data_raw\\350_weatherfile_2021.xlsx')
@@ -53,7 +53,7 @@ ax1.xaxis.set_major_locator(mdates.MonthLocator())
 ax1.set_ylabel('Temperature (°C)')
 ax1.set_xticklabels('')
 
-ax1.set_title('Indian Weather Data')
+ax1.set_title('India')
 ax1.text(indicatation_text_x, indicatation_text_y, 'b)', transform=ax1.transAxes, size=config.subplot_fs, weight='bold')
 
 ax2 = fig.add_subplot(gs[0, 0], sharey=ax1)
@@ -64,40 +64,40 @@ ax2.plot(df_NL.index, df_NL['TMIN'], label='Minimum Temperature', color='g')
 ax2.set_xticklabels('')
 ax2.set_ylabel('Temperature (°C)')
 ax2.legend()
-ax2.set_title('Dutch Weather Data')
+ax2.set_title('The Netherlands')
 ax2.text(indicatation_text_x, indicatation_text_y, 'a)', transform=ax2.transAxes, size=config.subplot_fs, weight='bold')
 
 
 # Second subplot
 # Third subplot
-ax3 = fig.add_subplot(gs[1, 1])
-ax3.plot(df_IND.index, df_IND['IRRAD']/j_to_mj, label='Irraditation', color='b')
-ax3.xaxis.set_major_locator(mdates.MonthLocator())
-ax3.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
-# add day length 
-daylength_data.df_ind['daylength'].plot(label='Daylength', color='black', ax=ax3)
-# Create a second y-axis for the third subplot
-ax3b = ax3.twinx()
-ax3b.bar(df_IND.index, df_IND['RAIN'] * 10, label='Rain', color='r')
-ax3b.set_ylabel('Rainfall (mm)', color='r')
+# ax3 = fig.add_subplot(gs[1, 1])
+# ax3.plot(df_IND.index, df_IND['IRRAD']/j_to_mj, label='Irraditation', color='b')
+# ax3.xaxis.set_major_locator(mdates.MonthLocator())
+# ax3.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+# # add day length 
+# daylength_data.df_ind['daylength'].plot(label='Daylength', color='black', ax=ax3)
+# # Create a second y-axis for the third subplot
+# ax3b = ax3.twinx()
+# ax3b.bar(df_IND.index, df_IND['RAIN'] * 10, label='Rain', color='r')
+# ax3b.set_ylabel('Rainfall (mm)', color='r')
 
-ax3.text(indicatation_text_x, indicatation_text_y, 'd)', transform=ax3.transAxes, size=config.subplot_fs, weight='bold')
-ax3.legend()
+# ax3.text(indicatation_text_x, indicatation_text_y, 'd)', transform=ax3.transAxes, size=config.subplot_fs, weight='bold')
+# ax3.legend()
 
 
-ax4 = fig.add_subplot(gs[1, 0], sharey=ax3)
-ax4.plot(df_NL.index, df_NL['IRRAD']/j_to_mj, label='IRRAD', color='b')
-daylength_data.df_nl['daylength'].plot(label='Daylength', color='black', ax=ax4)
-ax4.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
-# ax4.set_ylabel('Irradiation', color='b')
-ax4b = ax4.twinx()
-ax4b.get_shared_y_axes().join(ax4b, ax3b)
-ax4b.bar(df_NL.index, df_NL['RAIN'] * 10, label='Rain', color='r')
-# Add letter indication
-ax4.text(indicatation_text_x, indicatation_text_y, 'c)', transform=ax4.transAxes, size=config.subplot_fs, weight='bold')
-ax4.set_ylabel('Irradiation (MJ/m²/day)', color='b')
-ax4b.legend()
-ax4b.set_yticklabels('')
+# ax4 = fig.add_subplot(gs[1, 0], sharey=ax3)
+# ax4.plot(df_NL.index, df_NL['IRRAD']/j_to_mj, label='IRRAD', color='b')
+# daylength_data.df_nl['daylength'].plot(label='Daylength', color='black', ax=ax4)
+# ax4.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+# # ax4.set_ylabel('Irradiation', color='b')
+# ax4b = ax4.twinx()
+# ax4b.get_shared_y_axes().join(ax4b, ax3b)
+# ax4b.bar(df_NL.index, df_NL['RAIN'] * 10, label='Rain', color='r')
+# # Add letter indication
+# ax4.text(indicatation_text_x, indicatation_text_y, 'c)', transform=ax4.transAxes, size=config.subplot_fs, weight='bold')
+# ax4.set_ylabel('Irradiation (MJ/m²/day)', color='b')
+# ax4b.legend()
+# ax4b.set_yticklabels('')
 
 plt.savefig(f'../output/weather_data.png', dpi = 300, bbox_inches='tight')
 plt.savefig(f'../output/weather_data.svg', dpi = 600, bbox_inches='tight')
