816f285a989ad33cd51806ecf5714c6f71d421d3
Jian <jian1.liu@wur.nl>
2024-07-19 12:04:28 +0200

    tidy up the code for lsa graph

--- scripts/graphs_for_paper.py
+++ scripts/graphs_for_paper.py
@@ -138,16 +138,19 @@ large_df['value'] = large_df['value'].astype(float)
 colors = config.name_color_map
 no_ofdays = len(large_df.day.unique())
 # %%
+# $t_{b\_pheno}$, TSUM1, TDWI, SPAN and $t_{phot-max}$.
 DAPs = np.tile(np.arange(no_ofdays), config.LSA_sample_size * len(para))
 large_df['DAP'] = DAPs[:len(large_df)]
 large_df.set_index('DAP', inplace=True)
 param_name = 't1_pheno'
 output_var = 'LAI'
 output_df = large_df[large_df['key'] == param_name].sort_values('day')
-param_name_no_effect = 'RGRLAI'
+param_name_no_effect = 'TSUM1'
 output_df_no_effect = large_df[large_df['key'] == param_name_no_effect].sort_values('day')
 param_name_wirdo = 'te'
 output_df_wirdo = large_df[large_df['key'] == param_name_wirdo].sort_values('day')
+SPAN = large_df[large_df['key'] == 'SPAN'].sort_values('day')
+TDWI = large_df[large_df['key'] == 'TDWI'].sort_values('day')
 ylimt_upper = 6 # output_df.LAI.max() + 0.5
 xlimt_upper = no_ofdays - 1
 pointsize = 1
@@ -159,107 +162,64 @@ param_name_wirdo = config.label_map.get(param_name_wirdo, param_name_wirdo)
 # DVS values 
 emergence_date = output_df[output_df['DVS'] == 0]['DVS'].drop_duplicates()
 tuberintiation_date = output_df[output_df['DVS'] == 1]['DVS'].drop_duplicates()
+
+# %%
+
+import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import numpy as np
 
-fig = plt.figure(figsize=(7, 9))
+def create_plots(config, output_df, output_df_wirdo, output_df_no_effect, SPAN, TDWI, param_name, param_name_wirdo, param_name_no_effect, output_var, pointsize, emergence_date, tuberintiation_date, subplotlab_x, subplotlab_y, ylimt_upper, no_ofdays):
+    plt.rcParams.update({'font.size': config.subplot_fs})  # Adjust the font size as needed
 
-# Create a GridSpec for the whole figure
-gs = gridspec.GridSpec(3, 2, figure=fig, hspace=0.3, wspace=0.3)
+    fig = plt.figure(figsize=(7, 9))
 
-# First subplot
-ax1 = fig.add_subplot(gs[0, 0])
-cmap = plt.get_cmap('viridis')
-norm = plt.Normalize(output_df['value'].min(), output_df['value'].max())
-sc = ax1.scatter(output_df.index, output_df[output_var], c=output_df['value'], s = pointsize, cmap=cmap, norm=norm)
-# fig.colorbar(sc, ax=ax1)
-ax1.set_xlabel('')
-ax1.set_ylabel(output_var)
-ax1.axvline(emergence_date.index, color='green', linestyle='-')
-ax1.axvline(tuberintiation_date.index, color='green', linestyle='-')
-ax1.set_ylim(0, ylimt_upper)
-# ax1.set_xlim(0, xlimt_upper)
-ax1.vlines(xlimt_upper, 0, ylimt_upper, color='red', linestyle='--')
-# ax1.annotate('Illustration on the right', xy=(xlimt_upper, ylimt_upper),
-#               xytext=(xlimt_upper, ylimt_upper + 1), 
-#              arrowprops=dict(facecolor='black', shrink=0.0), ha = 'center')
-ax1.text(subplotlab_x, subplotlab_y, 'a)', transform=ax1.transAxes, size=config.subplot_fs, weight='bold')
-# Second subplot
-ax2 = fig.add_subplot(gs[0, 1])
-# Get the final day of output_df
-final_day_df = output_df[output_df.index == output_df.index.max()]
+    # Create a GridSpec for the whole figure
+    gs = gridspec.GridSpec(3, 2, figure=fig, hspace=0.3, wspace=0.5)
 
-# Plot LAI against parameter values
-sc2 = ax2.scatter(final_day_df['value'], final_day_df[output_var], c=final_day_df['value'], s = pointsize + 4, cmap=cmap, norm=norm)
-fig.colorbar(sc2, ax=ax2)
-ax2.set_xlabel(param_name)
-ax2.set_ylabel(output_var)
-ax2.text(subplotlab_x, subplotlab_y, 'b)', transform=ax2.transAxes, size=config.subplot_fs, weight='bold')
-# Third subplot
-ax3 = fig.add_subplot(gs[1, 0])
-cmap = plt.get_cmap('viridis')
-norm = plt.Normalize(output_df_wirdo['value'].min(), output_df_wirdo['value'].max())
-sc3 = ax3.scatter(output_df_wirdo.index, output_df_wirdo[output_var], c=output_df_wirdo['value'], s = pointsize + 4, cmap=cmap, norm=norm)
-# fig.colorbar(sc, ax=ax1)
-ax3.set_xlabel('')
-ax3.set_ylabel(output_var)
-ax3.set_ylim(0, ylimt_upper)
-# ax3.set_xlim(0, xlimt_upper)
-xlimt_upper = (no_ofdays - 1)/2 #if config.run_NL_conditions else no_ofdays - 1
-ax3.vlines(xlimt_upper, 0, ylimt_upper, color='red', linestyle='--')
-# ax3.annotate('Illustration on the right', xy=(xlimt_upper, ylimt_upper),
-#               xytext=(xlimt_upper, ylimt_upper + 1), 
-#              arrowprops=dict(facecolor='black', shrink=0.0), ha = 'center')
-ax3.text(subplotlab_x, subplotlab_y, 'c)', transform=ax3.transAxes, size=config.subplot_fs, weight='bold')
+    def create_subplot(ax, data, output_var, param_name, label, index_label, pointsize, ylimt_upper, subplot_label):
+        cmap = plt.get_cmap('viridis')
+        norm = plt.Normalize(data['value'].min(), data['value'].max())
+        sc = ax.scatter(data.index, data[output_var], c=data['value'], s=pointsize, cmap=cmap, norm=norm)
+        fig.colorbar(sc, ax=ax, label=param_name)
+        ax.set_xlabel(index_label)
+        ax.set_ylabel(label)
+        ax.set_ylim(0, ylimt_upper)
+        ax.text(subplotlab_x, subplotlab_y, subplot_label, transform=ax.transAxes, size=config.subplot_fs, weight='bold')
 
-# Fourth subplot
-# from mpl_toolkits.axes_grid1 import make_axes_locatable
+    # First subplot
+    ax1 = fig.add_subplot(gs[0, 0])
+    create_subplot(ax1, output_df, output_var, param_name, output_var, '', pointsize, ylimt_upper, 'a)')
+    ax1.axvline(emergence_date.index, color='green', linestyle='-')
+    ax1.axvline(tuberintiation_date.index, color='green', linestyle='-')
 
-# Fourth subplot
-ax4 = fig.add_subplot(gs[1, 1])
-# Get the day of output_df for x and y scatter plot
-selected_day = round((no_ofdays - 1)/2) # if config.run_NL_conditions else no_ofdays - 1
-final_day_df_wirdo = output_df_wirdo[output_df_wirdo.index == selected_day]
+    # Second subplot
+    ax3 = fig.add_subplot(gs[1, 1])
+    create_subplot(ax3, output_df_wirdo, output_var, param_name_wirdo, '', 'DAP', pointsize + 4, ylimt_upper, 'd)')
 
-# Plot LAI against parameter values
-sc4 = ax4.scatter(final_day_df_wirdo['value'], final_day_df_wirdo[output_var], c=final_day_df_wirdo['value'], s = pointsize + 4, cmap=cmap, norm=norm)
-fig.colorbar(sc4, ax=ax4)
-ax4.set_xlabel(param_name_wirdo)
-ax4.set_ylabel(output_var)
-ax4.text(subplotlab_x, subplotlab_y, 'd)', transform=ax4.transAxes, size=config.subplot_fs, weight='bold')
 
-# Fifth subplot output_df_wirdo
-ax5 = fig.add_subplot(gs[2, 0])
-cmap = plt.get_cmap('viridis')
-norm = plt.Normalize(output_df_no_effect['value'].min(), output_df_no_effect['value'].max())
-sc5 = ax5.scatter(output_df_no_effect.index, output_df_no_effect[output_var], c=output_df_no_effect['value'], s = pointsize + 4, cmap=cmap, norm=norm)
-# fig.colorbar(sc, ax=ax1)
-ax5.set_xlabel('DAP')
-ax5.set_ylabel(output_var)
-ax5.text(subplotlab_x, subplotlab_y, 'e)', transform=ax5.transAxes, size=config.subplot_fs, weight='bold')
-xlimt_upper = (no_ofdays - 1)/2 if config.run_NL_conditions else no_ofdays - 1
-ax5.vlines(xlimt_upper, 0, ylimt_upper, color='red', linestyle='--')
-# ax5.annotate('Illustration on the right', xy=(xlimt_upper, ylimt_upper ),
-            #   xytext=(xlimt_upper, ylimt_upper + 1), 
-            #  arrowprops=dict(facecolor='black', shrink=0.0), ha = 'center')
-ax5.set_ylim(0, ylimt_upper)
-# ax5.set_xlim(0, xlimt_upper)
-# Sixth subplot
-ax6 = fig.add_subplot(gs[2, 1])
-# Get the final day of output_df
-final_day_df_no_effect = output_df_no_effect[output_df_no_effect.index == selected_day]
+    # Third subplot
+    ax5 = fig.add_subplot(gs[1, 0])
+    create_subplot(ax5, output_df_no_effect, output_var, param_name_no_effect, output_var, '', pointsize + 4, ylimt_upper, 'e)')
 
-# Plot LAI against parameter values
-sc6 = ax6.scatter(final_day_df_no_effect['value'], final_day_df_no_effect[output_var], c=final_day_df_no_effect['value'], s = pointsize + 4, cmap=cmap, norm=norm)
-fig.colorbar(sc6, ax=ax6)
-ax6.set_xlabel(param_name_no_effect)
-ax6.set_ylabel(output_var)
-ax6.text(subplotlab_x, subplotlab_y, 'f)', transform=ax6.transAxes, size=config.subplot_fs, weight='bold')
+    # Fourth subplot
+    ax6 = fig.add_subplot(gs[2, 0])
+    create_subplot(ax6, SPAN, output_var, 'SPAN', output_var, 'DAP', pointsize + 4, ylimt_upper, 'c)')
 
-scenario = "NL_" if config.run_NL_conditions else ""
-plt.savefig(f'{config.p_out_LSA}/{scenario}{output_var}_mainText_{config.LSA_sample_size}.png', dpi = 300, bbox_inches='tight', pad_inches=0.1)
-plt.savefig(f'{config.p_out_LSA}/{scenario}{output_var}_mainText_{config.LSA_sample_size}.svg', bbox_inches='tight', pad_inches=0.1)
-plt.show()
+    # Fifth subplot
+    ax4 = fig.add_subplot(gs[0, 1])
+    create_subplot(ax4, TDWI, output_var, 'TDWI', '', '', pointsize + 4, ylimt_upper, 'b)')
 
+    # Save the figure
+    scenario = "NL_" if config.run_NL_conditions else ""
+    output_path = f'{config.p_out_LSA}/{scenario}{output_var}_mainText_{config.LSA_sample_size}'
+    plt.savefig(f'{output_path}.png', dpi=300, bbox_inches='tight', pad_inches=0.1)
+    plt.savefig(f'{output_path}.svg', bbox_inches='tight', pad_inches=0.1)
+    plt.show()
+
+create_plots(config, output_df, output_df_wirdo, output_df_no_effect, SPAN, TDWI, param_name, param_name_wirdo, param_name_no_effect, output_var, pointsize, emergence_date, tuberintiation_date, subplotlab_x, subplotlab_y, ylimt_upper, no_ofdays)
+
+plt.rcParams['font.size'] = original_font_size
 
 #  BACK TO GSA VISUALISATION
 # %% # legend to rename and italicise
