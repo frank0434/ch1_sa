28cf312bdf4dcde90b5923f2a27c3f5a1e8ce5e2
Jian <jian1.liu@wur.nl>
2024-07-23 12:50:01 +0200

    Manual DVS legend

--- scripts/graphs_for_paper.py
+++ scripts/graphs_for_paper.py
@@ -18,12 +18,12 @@ emergence_date, tuber_initiation = process_dvs_files()
 import RankingOverSeason as ros
 
 base_path = "C:/Users/liu283/GitRepos/ch1_SA/"
-col_variable = "TWSO" 
+col_variable = "DVS" 
 file = os.path.join(base_path, f"output_NL_AUC_{col_variable}.csv") if config.run_NL_conditions else os.path.join(base_path, f"output_AUC_{col_variable}.csv")
 df_pawn_ros, df_st_ros = ros.process_AUC_file(file)
 df_ros = pd.merge(df_st_ros, df_pawn_ros, how='inner', on=['variable','label','country'])
 
-col = 'TWSO'
+col = 'DVS'
 df_sensitivity_S1, df_sensitivity_ST = process_files(col)
 df_pawn_long = create_dataframe_from_dict(load_PAWN(col))
 df_pawn_long = df_pawn_long[df_pawn_long['median'] > Dummy_si[1][1]]
@@ -39,6 +39,8 @@ if col in ['LAI', 'TWSO']:
     start_date = emergence_date[0] if col == 'LAI' else tuber_initiation[0]
     df_sensitivity_ST = df_sensitivity_ST.iloc[start_date:]
     df_pawn = df_pawn_median.iloc[start_date:]
+else:
+    df_pawn = df_pawn_median
 
 df2 = normalize_sensitivity(df_sensitivity_ST)
 df3 = normalize_sensitivity(df_pawn)
@@ -97,6 +99,17 @@ plt.savefig('../output/legend_graph.svg', format='svg')
 # Show the plot
 plt.show()
 
+# %% Legend for DVS
+fig, ax = plt.subplots(figsize=(12.5, 1), frameon=False)
+ax.axis('off')  # Hide the axes
+DVS_params = ['t1_pheno', 'TSUM1', 'TSUM2', 'TSUMEM', 'TBASEM', 'TEFFMX'] 
+colors_DVS = [config.name_color_map.get(col, 'black') for col in DVS_params]
+labels_DVS = [config.label_map.get(label, label) for label in DVS_params]
+lines_DVS = [plt.Line2D([0], [0], color=c, linewidth=15, linestyle='-') for c in colors_DVS]
+fig.legend(lines_DVS, labels_DVS, loc='center', ncol=6, handlelength = 1, handleheight = 2, borderpad=1,
+              markerscale = 3, handletextpad=1, columnspacing = 1.5, fontsize=config.subplot_fs, frameon=False)
+plt.savefig('../output/legend_DVS.svg', format='svg')
+plt.show()
 
 
 
@@ -137,11 +150,25 @@ large_df = pd.concat(dfs)
 large_df['value'] = large_df['value'].astype(float)
 colors = config.name_color_map
 no_ofdays = len(large_df.day.unique())
+
+# dfs_IND = []  # List to store DataFrames
+# for i, key, value in zip(para_vals.keys(), keys, para_vals.values()):
+#     # print(i, value)
+#     with open(f'C:/Users/liu283/GitRepos/ch1_SA/output/LSA/sims_100/{i}_{key}.json', 'r') as f:
+#         results = json.load(f)
+#     df = pd.DataFrame(results)
+#     df['key'] = key
+#     df['value'] = value
+#     dfs_IND.append(df)
+# large_df_IND = pd.concat(dfs_IND)
+# large_df_IND['value'] = large_df_IND['value'].astype(float)
+
 # %%
 # $t_{b\_pheno}$, TSUM1, TDWI, SPAN and $t_{phot-max}$.
 DAPs = np.tile(np.arange(no_ofdays), config.LSA_sample_size * len(para))
 large_df['DAP'] = DAPs[:len(large_df)]
 large_df.set_index('DAP', inplace=True)
+
 param_name = 't1_pheno'
 output_var = 'LAI'
 output_df = large_df[large_df['key'] == param_name].sort_values('day')
@@ -172,47 +199,47 @@ import numpy as np
 def create_plots(config, output_df, output_df_wirdo, TSUM1, SPAN, TDWI, param_name, param_name_wirdo, param_name_no_effect, output_var, pointsize, emergence_date, tuberintiation_date, subplotlab_x, subplotlab_y, ylimt_upper, no_ofdays):
     plt.rcParams.update({'font.size': config.subplot_fs})  # Adjust the font size as needed
 
-    fig = plt.figure(figsize=(3, 15))
+    fig = plt.figure(figsize=(4, 12))
 
     # Create a GridSpec for the whole figure
-    gs = gridspec.GridSpec(5, 1, figure=fig, hspace=0.1, wspace=0.3)
+    gs = gridspec.GridSpec(5, 1, figure=fig, hspace=0.2, wspace=0.3)
 
     def create_subplot(ax, data, output_var, param_name, label, index_label, pointsize, ylimt_upper, subplot_label):
         cmap = plt.get_cmap('viridis')
         norm = plt.Normalize(data['value'].min(), data['value'].max())
         sc = ax.scatter(data.index, data[output_var], c=data['value'], s=pointsize, cmap=cmap, norm=norm)
-        if not config.run_NL_conditions:
-            fig.colorbar(sc, ax=ax, label=param_name)
+        # if not config.run_NL_conditions:
+        fig.colorbar(sc, ax=ax, label=param_name)
         
         ax.set_xlabel('')
         # ax.set_ylabel(label)
         ax.set_ylim(0, ylimt_upper)
-        ax.text(subplotlab_x, subplotlab_y, subplot_label, transform=ax.transAxes, size=config.subplot_fs, weight='bold')
+        ax.text(subplotlab_x, subplotlab_y - 0.1, subplot_label, transform=ax.transAxes, size=config.subplot_fs, weight='bold')
 
     # First subplot
     ax1 = fig.add_subplot(gs[0, 0])
-    create_subplot(ax1, output_df, output_var, param_name, output_var, '', pointsize, ylimt_upper, 'a)')
+    create_subplot(ax1, output_df, output_var, param_name, output_var, '', pointsize, ylimt_upper, 'f)')
     ax1.axvline(emergence_date.index, color='green', linestyle='-')
     ax1.axvline(tuberintiation_date.index, color='green', linestyle='-')
     ax1.set_xticklabels('')
     # Second subplot
     ax2 = fig.add_subplot(gs[1, 0])
-    create_subplot(ax2, TSUM1, output_var, param_name_no_effect, output_var, '', pointsize + 4, ylimt_upper, 'b)')
+    create_subplot(ax2, TSUM1, output_var, param_name_no_effect, output_var, '', pointsize + 4, ylimt_upper, 'g)')
     ax2.set_xticklabels('')
     # Third subplot
     ax3 = fig.add_subplot(gs[2, 0])
-    create_subplot(ax3, SPAN, output_var, 'SPAN', output_var, 'DAP', pointsize + 4, ylimt_upper, 'c)')
+    create_subplot(ax3, SPAN, output_var, 'SPAN', output_var, 'DAP', pointsize + 4, ylimt_upper, 'h)')
     ax3.set_xticklabels('')
     # Fourth subplot
     ax4 = fig.add_subplot(gs[3, 0])
-    create_subplot(ax4, TDWI, output_var, 'TDWI', '', '', pointsize + 4, ylimt_upper, 'd)')
+    create_subplot(ax4, TDWI, output_var, 'TDWI', '', '', pointsize + 4, ylimt_upper, 'i)')
     ax4.set_xticklabels('')
     # Fifth subplot
     ax5 = fig.add_subplot(gs[4, 0])
-    create_subplot(ax5, output_df_wirdo, output_var, param_name_wirdo, '', 'DAP', pointsize + 4, ylimt_upper, 'e)')
+    create_subplot(ax5, output_df_wirdo, output_var, param_name_wirdo, '', 'DAP', pointsize + 4, ylimt_upper, 'j)')
     ax5.set_xlabel('DAP')
-    if config.run_NL_conditions:
-        fig.text(-0.05, 0.5, 'Leaf Area Index', va='center', rotation='vertical', fontsize=config.subplot_fs)
+    # if config.run_NL_conditions:
+    fig.text(0, 0.5, 'Leaf Area Index', va='center', rotation='vertical', fontsize=config.subplot_fs)
 
     # Save the figure
     scenario = "NL_" if config.run_NL_conditions else ""
