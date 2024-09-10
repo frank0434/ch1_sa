6cd7cc05091202385c8020614727d9e9a1849af2
jian liu <jian1.liu@wur.nl>
2024-09-10 16:46:28 +0200

    move the last fig in supplementray to the graph script

--- scripts/graphs_for_paper.py
+++ scripts/graphs_for_paper.py
@@ -533,3 +533,42 @@ days_below_15_or_above_24_first_half_weather_nl = first_half_weather_nl[(first_h
 emergence_nl = 24
 print(f"Number of days with temperature below 15 or above 24 in the first half of weather_nl DataFrame: {days_below_15_or_above_24_first_half_weather_nl - emergence_nl}")
 # %%
+# FigS1.17 The last figure 
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json
+
+# %%
+with open('../output/daysims_32768/day_105.json', 'r') as f:
+    data = json.load(f)
+# %%
+day105 = pd.DataFrame(data).T
+# %%
+# Set a theme
+sns.set_theme()
+
+fig, ax = plt.subplots(3, 1, figsize=(6, 8))
+
+# Define the column names and their corresponding titles
+columns = ['DVS', 'LAI', 'TWSO']
+titles = ['DVS', 'LAI', 'TWSO']
+
+for i, (col, title) in enumerate(zip(columns, titles)):
+    # Use seaborn for a nicer histogram
+    sns.histplot(day105[col], bins=100, kde=True, ax=ax[i])
+    
+    # Set the title and labels
+    ax[i].set_title(col, fontsize=14)
+    ax[i].set_xlabel('Value', fontsize=12)
+    ax[i].set_ylabel('Frequency', fontsize=12)
+
+# Improve layout
+fig.tight_layout()
+
+# Save the figure
+fig.savefig('../output/Distribution_105_TWSO.png', dpi=300)
+fig.savefig('../output/Distribution_105_TWSO.svg')
+
+plt.show()
